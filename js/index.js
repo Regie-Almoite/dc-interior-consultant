a046ed88539ddb32523f19df8957d93f28c6e936
Regie-Almoite <regienaldalmoite@yahoo.com>
2022-12-20 07:24:39 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+const hamburgerBtn = document.getElementById("hamburger");
+
+console.log(hamburgerBtn);
+hamburgerBtn.addEventListener("click", function () {
+    alert("this");
+});
